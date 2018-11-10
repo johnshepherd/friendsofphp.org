c49ea112cac4b95e2d67ced6902c3ca5dbc97e27
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-10 19:42:26 +0100

    Deploy TomasVotruba/friendsofphp.org to github.com/TomasVotruba/friendsofphp.org.git:gh-pages

---
+++ assets/js/menu-area-pick.js
@@ -0,0 +1,37 @@
+// inspired by https://github.com/JanMikes/tomasvotruba.cz/blob/be9da66c3402adfe7928c3798ab1ccd6527f92cd/source/assets/js/checklist.js
+
+$(function() {
+    var showRowsInBounds = function(bounds) {
+        $("tr.meetup").each(function () {
+            var meetupLatLng = L.latLng($(this).data('latitude'), $(this).data('longitude'));
+            if (bounds.contains(meetupLatLng)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+
+        // see https://stackoverflow.com/a/20064911/1348344
+        var visibleMeetups = $("table#table-meetups tr:visible").length - 1;
+
+        console.log(visibleMeetups);
+
+        if (visibleMeetups === 0) {
+            $("#table-meetups").hide();
+            $("#block-zoomout").show();
+        } else {
+            $("#table-meetups").show();
+            $("#block-zoomout").hide();
+        }
+    };
+
+    // show relevant meetups when map moves
+    map.on('moveend', function() {
+        showRowsInBounds(map.getBounds());
+    });
+
+    var pastBounds = window.localStorage.getItem('bounds');
+    if (pastBounds) {
+        showRowsInBounds(restoreBoundFromString(pastBounds));
+    }
+});
